2a355c0534f449284753f06a6cfb4746f5e62fc8
Hemant Singh <hsingh@impact-guard.com>
2024-02-09 08:09:33 -0500

    commit

---
+++ src/app/ui/generic-card/generic-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './generic-card.component.html',
+  styleUrl: './generic-card.component.scss'
+})
+export class GenericCardComponent {
+
+}
